d90fd2eb7a95470afa256994d626ecf68ee2b452
RyomenDev <aakashvatsh007@gmail.com>
2024-12-08 18:07:16 +0530

    cors-origin-1

--- server/index.js
+++ server/index.js
@@ -8,15 +8,35 @@ require("dotenv");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const allowedOrigins1 = process.env.ALLOWED_ORIGIN1;
-const allowedOrigins2 = process.env.ALLOWED_ORIGIN2;
-// console.log(allowedOrigins.split(","));
-console.log(allowedOrigins1, allowedOrigins2);
+// const allowedOrigins1 = process.env.ALLOWED_ORIGIN1;
+// const allowedOrigins2 = process.env.ALLOWED_ORIGIN2;
+// // console.log(allowedOrigins.split(","));
+// console.log(allowedOrigins1, allowedOrigins2);
 
 // CORS Middleware
-app.use(
-  cors('*')
-);
+// app.use(cors("*"));
+const allowedOrigins = [
+  process.env.ALLOWED_ORIGIN1,
+  process.env.ALLOWED_ORIGIN2,
+  // Add other allowed origins here
+];
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    // Allow requests with no origin (like mobile apps or curl requests)
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("CORS policy: Access denied"), false);
+    }
+  },
+  methods: ["GET", "POST", "PUT", "DELETE"], // specify allowed methods
+  credentials: true, // if you need to send cookies or authorization headers
+};
+
+// Apply CORS middleware
+app.use(cors(corsOptions));
+
 
 // Middleware to parse JSON request bodies
 app.use(bodyParser.json());
